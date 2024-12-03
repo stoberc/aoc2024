7f0ffe8eabef0a900331c646d910cd278d28c167
Stober <CHRISTIAN_STOBER@dpsk12.org>
2024-12-02 22:42:56 -0700

    Reimplemented p3 with cleaner regex use

--- p3.py
+++ p3.py
@@ -1,12 +1,13 @@
-import pdb
 import re
 
 FNAME = "in3.txt"
 
 stream = open(FNAME).read()
 
-commands = re.findall("mul\(\-?\d+,-?\d+\)", stream)
+# extract all the legitimate mul commands
+commands = re.findall("mul\(\-?\d+,-?\d+\)", stream) # e.g. mul(23, 40)
     
+# loop through them evaluating and summing
 total = 0
 for i in commands:
     a, b = re.findall('-?\d+', i)
@@ -14,28 +15,19 @@ for i in commands:
 
 print("Part 1:", total)
 
-commands = re.findall("(mul\(\-?\d+,-?\d+\))|(do\(\))|(don't\(\))", stream)
+# now include the do and don't commands
+commands = re.findall("(mul\(\-?\d+,-?\d+\)|do\(\)|don't\(\))", stream)
 
-filtered_commands = []
-for a, b, c in commands:
-    if a:
-        filtered_commands.append(a)
-    elif b:
-        filtered_commands.append(b)
-    elif c:
-        filtered_commands.append(c)
-
+# same idea as before, except "don't" disables counting until "do()"
 total = 0
-count = True
-for command in filtered_commands:
-    if command == 'do()':
+count = True # initially enabled
+for command in commands:
+    if command == "do()":
         count = True
     elif command == "don't()":
         count = False
-    elif count:
+    elif count: # must be a mul
         a, b = re.findall('-?\d+', command)
         total += int(a) * int(b)
 
 print("Part 2:", total)
-
-pdb.set_trace()
