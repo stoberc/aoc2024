7ff378f23d91b315bff8e22de54c83497336255d
Stober <CHRISTIAN_STOBER@dpsk12.org>
2024-12-12 22:33:32 -0700

    Removed vestigial approach from Day 13

--- p13.py
+++ p13.py
@@ -8,18 +8,6 @@ def parse_chunk(line):
 chunks = open(FNAME).read().split('\n\n')
 machines = [parse_chunk(chunk) for chunk in chunks]
 
-# initial approach--brute force looping over possible numbers of B presses
-def min_cost(dxa, dya, dxb, dyb, tx, ty):
-    maxb_presses = min(tx // dxb, ty // dyb)
-    for i in range(maxb_presses, -1, -1):
-        rx = tx - i * dxb
-        ry = ty - i * dyb
-        if rx % dxa == 0 and ry % dya == 0 and rx // dxa == ry // dya:
-            return 3 * rx // dxa + i
-    return 0
-    
-print("Part 1:", sum([min_cost(*machine) for machine in machines]))
-
 # revised approach, algebra--solve for coefficients
 # apparently is a property of the input the input that all A vectors and B vectors are linearly independent
 # or else this would trigger some division by zero errors
